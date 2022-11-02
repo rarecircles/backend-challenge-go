995389c99fe5efb228819e9b336ba2d2b76d315d
Ted Kim <dev.ted.kim@gmail.com>
2022-11-02 13:08:56 -0400

    clean main code

--- cmd/challenge/main.go
+++ cmd/challenge/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/elastic/go-elasticsearch/v8"
@@ -42,7 +41,7 @@ func run(log *zap.Logger) error {
 	rpcAPIKey := os.Getenv("RPC_API_KEY")
 	filePath := os.Getenv("ADDRESS_FILE_PATH")
 	esURL := os.Getenv("ELASTICSEARCH_URL")
-	seedData := os.Getenv("SEED_DATA")
+	seedDataOn := os.Getenv("SEED_DATA")
 
 	rpc.SetLogger(log)
 	eth.SetLogger(log)
@@ -55,69 +54,9 @@ func run(log *zap.Logger) error {
 		log.Fatal("failed to connect elastic search: " + err.Error())
 	}
 
-	if seedData == "true" {
+	if seedDataOn == "true" {
 		log.Info("Start to seed data")
-		addrCh := make(chan string, numJobs)
-		ethTokenCh := make(chan *eth.Token, numJobs)
-
-		al := addressLoader.NewAddressLoader(log, addrCh)
-		if al == nil {
-			log.Fatal("failed to create an address loader")
-		}
-
-		rpcClient := rpc.NewClient(rpcURL + rpcAPIKey)
-		if rpcClient == nil {
-			log.Fatal("failed to create a rpc client")
-		}
-
-		log.Info("Read an address file")
-		go func() {
-			if err := al.Load(filePath); err != nil {
-				log.Fatal("failed to load an address file: " + err.Error())
-			}
-		}()
-
-		log.Info("Get tokens from rpc")
-		go func() {
-			for address := range addrCh {
-				ethAddr, err := eth.NewAddress(address)
-				if err != nil {
-					log.Info("failed to create eth address " + err.Error())
-					continue
-				}
-				ethToken, err := rpcClient.GetERC20(ethAddr)
-				if err != nil {
-					log.Fatal("failed to get eth token " + err.Error())
-				}
-
-				ethTokenCh <- ethToken
-			}
-			close(ethTokenCh)
-		}()
-
-		log.Info("Seed eth tokens")
-		go func() {
-			for t := range ethTokenCh {
-				// TODO: TotalSupply is unsigned long or needs to be converted to string?
-				resp, err := esClient.Index(search.TokensIndex, esutil.NewJSONReader(t),
-					esClient.Index.WithDocumentID(t.Symbol))
-
-				if err != nil {
-					log.Error(err.Error())
-				}
-
-				if resp.IsError() {
-					log.Error("failed to index ",
-						zap.String("token", fmt.Sprintf("%+v", t)),
-					)
-				} else {
-					log.Error("success to index ",
-						zap.String("token", fmt.Sprintf("%+v", t)),
-					)
-				}
-				resp.Body.Close()
-			}
-		}()
+		seedData(filePath, rpcURL, rpcAPIKey, esClient)
 	}
 
 	log.Info("Running TOKEN-API",
@@ -136,4 +75,76 @@ func run(log *zap.Logger) error {
 	}
 
 	return nil
+}
+
+func seedData(filePath, rpcURL, rpcAPIKey string, esClient *elasticsearch.Client) {
+	addrCh := make(chan string, numJobs)
+	ethTokenCh := make(chan *eth.Token, numJobs)
+
+	al := addressLoader.NewAddressLoader(log, addrCh)
+	if al == nil {
+		log.Fatal("failed to create an address loader")
+	}
+
+	rpcClient := rpc.NewClient(rpcURL + rpcAPIKey)
+	if rpcClient == nil {
+		log.Fatal("failed to create a rpc client")
+	}
+
+	log.Info("Read an address file")
+	go readAddressFile(al, filePath)
+
+	log.Info("Get tokens from rpc")
+	go getTokensFromRPC(addrCh, ethTokenCh, rpcClient)
+
+	log.Info("Seed eth tokens")
+	go seedTokens(ethTokenCh, esClient)
+}
+
+func readAddressFile(al addressLoader.AddressLoader, filePath string) {
+	if err := al.Load(filePath); err != nil {
+		log.Fatal("failed to load an address file: " + err.Error())
+	}
+}
+
+func getTokensFromRPC(addrCh chan string, ethTokenCh chan *eth.Token, rpcClient *rpc.Client) {
+	for address := range addrCh {
+		ethAddr, err := eth.NewAddress(address)
+		if err != nil {
+			log.Info("failed to create eth address " + err.Error())
+			continue
+		}
+		ethToken, err := rpcClient.GetERC20(ethAddr)
+		if err != nil {
+			log.Info("failed to get eth token " + err.Error())
+			continue
+		}
+
+		ethTokenCh <- ethToken
+	}
+	close(ethTokenCh)
+}
+
+func seedTokens(ethTokenCh chan *eth.Token, esClient *elasticsearch.Client) {
+	for t := range ethTokenCh {
+		// TODO: TotalSupply is unsigned long or needs to be converted to string?
+		resp, err := esClient.Index(search.TokensIndex, esutil.NewJSONReader(t),
+			esClient.Index.WithDocumentID(t.Symbol))
+
+		if err != nil {
+			log.Error(err.Error())
+		}
+
+		// uncomment this to see tokens
+		// if resp.IsError() {
+		// 	log.Error("failed to index ",
+		// 		zap.String("token", fmt.Sprintf("%+v", t)),
+		// 	)
+		// } else {
+		// 	log.Error("success to index ",
+		// 		zap.String("token", fmt.Sprintf("%+v", t)),
+		// 	)
+		// }
+		resp.Body.Close()
+	}
 }
