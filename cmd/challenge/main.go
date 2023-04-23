a9def061ed2ba3986ac83f2f37da2d1687f1e08b
Amin Bashiri <amin.bashiri28@gmail.com>
2023-04-23 00:01:17 -0600

    put all the pieces together in main.go

--- cmd/challenge/main.go
+++ cmd/challenge/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
-	"flag"
+	"github.com/RediSearch/redisearch-go/redisearch"
+	"github.com/rarecircles/backend-challenge-go/eth"
+	"github.com/rarecircles/backend-challenge-go/eth/rpc"
+	"github.com/rarecircles/backend-challenge-go/internal/handlers"
+	"github.com/rarecircles/backend-challenge-go/internal/loader"
+	"github.com/rarecircles/backend-challenge-go/internal/repositories/ethrepo"
+	"github.com/rarecircles/backend-challenge-go/internal/repositories/redisrepo"
+	"github.com/rarecircles/backend-challenge-go/internal/repositories/tokensrepo"
+	"github.com/rarecircles/backend-challenge-go/internal/server"
+	"github.com/rarecircles/backend-challenge-go/internal/service"
 	"go.uber.org/zap"
 )
 
@@ -14,4 +23,27 @@ func main() {
 	rpc.SetLogger(zlog)
 	eth.SetLogger(zlog)
 
+	redisClient := redisearch.NewClient(config.RedisSearchUrl, config.RedisKeyPrefix)
+	redisRepo := redisrepo.New(zlog, redisClient)
+	err := redisRepo.CreateIndex()
+	if err != nil {
+		zlog.Fatal("failed to create index", zap.Error(err))
+	}
 
+	tokensRepo := tokensrepo.New(zlog, config.AddressesFile)
+	ethClient := rpc.NewClient(config.EthRpcUrl + config.EthRpcKey)
+	ethRepo := ethrepo.New(zlog, ethClient)
+	tokenLoader := loader.New(zlog, ethRepo, redisRepo, tokensRepo, config.NumWorkers, config.RefetchDelayHours)
+	tokenLoader.RunLoader()
+
+	svc := service.New(zlog, redisRepo)
+	hands := handlers.New(zlog, svc)
+	serv := server.New(zlog, hands)
+
+	zlog.Info("running Challenge",
+		zap.String("running server on port ", config.Port),
+	)
+
+	serv.Run(config.BaseUrl, config.Port)
+	zlog.Fatal("failed to run server")
+}
